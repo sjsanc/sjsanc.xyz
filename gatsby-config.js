af9c8250ce34babff281087f9cfec9857acbfb6f
sjsanc <sjsanc@protonmail.com>
2021-06-21 15:00:35 +0100

    removed analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,12 +4,6 @@ module.exports = {
   },
   plugins: [
     "gatsby-plugin-styled-components",
-    {
-      resolve: "gatsby-plugin-google-analytics",
-      options: {
-        trackingId: "",
-      },
-    },
     "gatsby-plugin-react-helmet",
     "gatsby-transformer-remark",
     {
