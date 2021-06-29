5d6e5d5f190dff6e18f6e8c758b8ea6a8c21cc17
sjsanc <52794806+sjsanc@users.noreply.github.com>
2021-06-29 09:28:42 +0100

    updated links + deploy.sh

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,22 +1,22 @@
 module.exports = {
-  siteMetadata: {
-    title: "sjsanc.xyz",
-  },
-  plugins: [
-    "gatsby-plugin-styled-components",
-    "gatsby-plugin-react-helmet",
-    "gatsby-transformer-remark",
-    "gatsby-plugin-sass",
-    "gatsby-plugin-image",
-    "gatsby-plugin-sharp",
-    "gatsby-transformer-sharp",
-    {
-      resolve: "gatsby-source-filesystem",
-      options: {
-        name: "pages",
-        path: "./src/pages/",
-      },
-      __key: "pages",
+    siteMetadata: {
+        title: "sjsanc.xyz"
     },
-  ],
+    plugins: [
+        "gatsby-plugin-styled-components",
+        "gatsby-plugin-react-helmet",
+        "gatsby-transformer-remark",
+        "gatsby-plugin-sass",
+        "gatsby-plugin-image",
+        "gatsby-plugin-sharp",
+        "gatsby-transformer-sharp",
+        {
+            resolve: "gatsby-source-filesystem",
+            options: {
+                name: "pages",
+                path: "./src/pages/"
+            },
+            __key: "pages"
+        }
+    ]
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,53 +4,58 @@ import { StaticImage } from "gatsby-plugin-image";
 import LogoImage from "../images/logo.png";
 
 const projects = [
-  {
-    name: "An Imperial Simulation",
-    description: "A strategy game about empires.",
-    link: "",
-  },
-  {
-    name: "Mnemonist",
-    description: "A guided platform for learning languages",
-    link: "",
-  },
-  {
-    name: "AloeCMS",
-    description: "A simple, detail-oriented CMS",
-    link: "",
-  },
+    {
+        name: "An Imperial Simulation",
+        description: "A strategy game about empires.",
+        link: "https://github.com/sjsanc/an-imperial-simulation"
+    },
+    {
+        name: "Mnemonist",
+        description: "A guided platform for learning languages",
+        link: ""
+    },
+    {
+        name: "AloeCMS",
+        description: "A simple, detail-oriented CMS",
+        link: ""
+    }
 ];
 
 const IndexPage = () => {
-  return (
-    <div class="container">
-      <title>Home Page</title>
-      <header>
-        <img src={LogoImage} id="logo" />
-        <h1>SJSANC.</h1>
-        <h2>UK-based software developer.</h2>
-        <h3>"People are fleeting; open source contributions are eternal."</h3>
-        <ul class="socials">
-          <li>
-            <a href="https://github.com/sjsanc">Github</a>
-          </li>
-          <li>
-            <a href="https://linkedin.com/in/steven-scheepers-72b45b131">Linkedin</a>
-          </li>
-        </ul>
-      </header>
-      <main>
-        <div class="projects">
-          {projects.map((proj) => (
-            <a class="project" href={proj.link}>
-              <h3>{proj.name}</h3>
-              <p>{proj.description}</p>
-            </a>
-          ))}
+    return (
+        <div class="container">
+            <title>Home Page</title>
+            <header>
+                <img src={LogoImage} id="logo" />
+                <h1>SJSANC.</h1>
+                <h2>UK-based software developer.</h2>
+                <h3>
+                    <em>"People are fleeting; open source contributions are eternal."</em>
+                </h3>
+                <ul class="socials">
+                    <li>
+                        <a href="https://github.com/sjsanc">Github</a>
+                    </li>
+                    <li>
+                        <a href="https://linkedin.com/in/steven-scheepers-72b45b131">Linkedin</a>
+                    </li>
+                </ul>
+            </header>
+            <main>
+                <div>
+                    <h2>Projects</h2>
+                    <div class="projects">
+                        {projects.map(proj => (
+                            <a class="project" href={proj.link}>
+                                <h3>{proj.name}</h3>
+                                <p>{proj.description}</p>
+                            </a>
+                        ))}
+                    </div>
+                </div>
+            </main>
         </div>
-      </main>
-    </div>
-  );
+    );
 };
 
 export default IndexPage;
