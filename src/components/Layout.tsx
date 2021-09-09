a2aeec668f7687a621e961018cc95917d4714be8
sjsanc <52794806+sjsanc@users.noreply.github.com>
2021-09-09 10:54:49 +0100

    Removed dead links, bio + added target to anchor

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -12,157 +12,151 @@ import theme from "../styles/theme";
 const pages = ["Projects", "Contact"];
 
 const Layout = (props: { children? }) => {
-  const [currentSection, setCurrentSection] = useState<string>();
-  const [expanded, setExpanded] = useState<boolean>();
+    const [currentSection, setCurrentSection] = useState<string>();
+    const [expanded, setExpanded] = useState<boolean>();
 
-  useEffect(() => {
-    let s = window.location.pathname.replace("/", "");
-    setCurrentSection(s.charAt(0).toUpperCase() + s.slice(1));
-  }, []);
+    useEffect(() => {
+        let s = window.location.pathname.replace("/", "");
+        setCurrentSection(s.charAt(0).toUpperCase() + s.slice(1));
+    }, []);
 
-  useEffect(() => {
-    currentSection !== "" ? setExpanded(true) : null;
-  }, []);
+    useEffect(() => {
+        currentSection !== "" ? setExpanded(true) : null;
+    }, []);
 
-  const changeSection = (evt) => {
-    const section = evt.target.innerText.toLowerCase();
-    const current = window.location.pathname.toLowerCase().replace("/", "");
+    const changeSection = (evt) => {
+        const section = evt.target.innerText.toLowerCase();
+        const current = window.location.pathname.toLowerCase().replace("/", "");
 
-    evt.preventDefault();
-    if (section == current) {
-      closeSection();
-    } else {
-      setExpanded(true);
-      navigate(`/${section}`);
-    }
-  };
+        evt.preventDefault();
+        if (section == current) {
+            closeSection();
+        } else {
+            setExpanded(true);
+            navigate(`/${section}`);
+        }
+    };
 
-  const closeSection = (): void => {
-    setExpanded(false);
-    setTimeout(() => {
-      navigate("/");
-    }, 200);
-  };
+    const closeSection = (): void => {
+        setExpanded(false);
+        setTimeout(() => {
+            navigate("/");
+        }, 200);
+    };
 
-  return (
-    <OuterWrapper id="outerWrapper">
-      <header>
-        <SEO />
-      </header>
-      <Menu locked={currentSection !== ""} id="menu">
-        <Logo src={LogoImage} alt="Portal key icon from Heroes of Newerth" />
-        <h1>SJSANC.</h1>
-        <p>
-          Hi -- welcome to my website. I'm a software engineer from the UK. I really like simulation
-          games.
-        </p>
-        <Links className="pages">
-          {pages.map((btn, i) => (
-            <a
-              className={btn == currentSection ? "activeLink" : ""}
-              key={i}
-              href={"/" + btn.toLowerCase()}
-              onClick={changeSection}>
-              {btn}
-            </a>
-          ))}
-        </Links>
-      </Menu>
-      {props.children ? (
-        <Slideout close={closeSection} expanded={expanded}>
-          {props.children}
-        </Slideout>
-      ) : null}
-      <Faded active={currentSection !== ""} id="fadedBackground" onClick={closeSection}></Faded>
-      <Background
-        style={{ backgroundImage: `url(${TileImage})` }}
-        id="patternedBackground"></Background>
-    </OuterWrapper>
-  );
+    return (
+        <OuterWrapper id="outerWrapper">
+            <header>
+                <SEO />
+            </header>
+            <Menu locked={currentSection !== ""} id="menu">
+                <Logo src={LogoImage} alt="Portal key icon from Heroes of Newerth" />
+                <h1>SJSANC.</h1>
+                <Links className="pages">
+                    {pages.map((btn, i) => (
+                        <a
+                            className={btn == currentSection ? "activeLink" : ""}
+                            key={i}
+                            href={"/" + btn.toLowerCase()}
+                            onClick={changeSection}>
+                            {btn}
+                        </a>
+                    ))}
+                </Links>
+            </Menu>
+            {props.children ? (
+                <Slideout close={closeSection} expanded={expanded}>
+                    {props.children}
+                </Slideout>
+            ) : null}
+            <Faded active={currentSection !== ""} id="fadedBackground" onClick={closeSection}></Faded>
+            <Background style={{ backgroundImage: `url(${TileImage})` }} id="patternedBackground"></Background>
+        </OuterWrapper>
+    );
 };
 
 const Faded = styled.div<{ active: boolean }>`
-  position: fixed;
-  inset: 0 0 0 0;
-  background: black;
-  opacity: ${(props) => (props.active ? 0.5 : 0)};
-  transition: 0.3s ease-in-out;
-  pointer-events: ${(props) => (props.active ? "" : "none")};
+    position: fixed;
+    inset: 0 0 0 0;
+    background: black;
+    opacity: ${(props) => (props.active ? 0.5 : 0)};
+    transition: 0.3s ease-in-out;
+    pointer-events: ${(props) => (props.active ? "" : "none")};
 `;
 
 const Logo = styled.img`
-  box-shadow: ${theme.boxShadow};
-  border-radius: 50%;
-  height: 64px;
-  width: 64px;
-  border: 5px solid white;
+    box-shadow: ${theme.boxShadow};
+    border-radius: 50%;
+    height: 64px;
+    width: 64px;
+    border: 5px solid white;
 `;
 
 const OuterWrapper = styled.div`
-  height: 100vh;
-  border: 15px solid ${theme.colors.dark};
-  overflow-y: scroll;
+    height: 100vh;
+    border: 15px solid ${theme.colors.dark};
+    overflow-y: scroll;
 `;
 
 const Background = styled.div`
-  min-height: 100vh;
-  height: 100%;
-  position: fixed;
-  inset: 0 0 0 0;
-  background-repeat: repeat;
-  background-size: 300px;
-  opacity: 0.1;
-  z-index: -1;
-  pointer-events: none;
+    min-height: 100vh;
+    height: 100%;
+    position: fixed;
+    inset: 0 0 0 0;
+    background-repeat: repeat;
+    background-size: 300px;
+    opacity: 0.1;
+    z-index: -1;
+    pointer-events: none;
 `;
 
 const Menu = styled.div<{ locked: boolean }>`
-  max-width: 400px;
-  padding: 64px;
-  overflow-y: ${(props) => (props.locked ? "hidden" : "auto")};
+    max-width: 400px;
+    padding: 64px;
+    overflow-y: ${(props) => (props.locked ? "hidden" : "auto")};
 
-  @media screen and (max-width: 992px) {
-    padding: 32px;
-  }
+    @media screen and (max-width: 992px) {
+        padding: 32px;
+    }
 
-  p,
-  h1 {
-    margin: 15px 0 15px 0;
-  }
+    p,
+    h1 {
+        margin: 15px 0 15px 0;
+    }
 
-  p {
-    background: ${theme.colors.dark};
-    color: white;
-    border-radius: 10px;
-    border: 5px solid white;
-    padding: 15px;
-    box-shadow: ${theme.boxShadow};
-  }
+    p {
+        background: ${theme.colors.dark};
+        color: white;
+        border-radius: 10px;
+        border: 5px solid white;
+        padding: 15px;
+        box-shadow: ${theme.boxShadow};
+    }
 
-  p::selection {
-    background: white;
-    color: ${theme.colors.dark};
-  }
+    p::selection {
+        background: white;
+        color: ${theme.colors.dark};
+    }
 `;
 
 const Links = styled.ul`
-  display: flex;
-  flex-direction: column;
-  align-items: flex-start;
-  a {
-    text-decoration: none;
-    margin: 10px 0;
-    position: relative;
-    font-size: 25px;
-    transition: 0.1s ease-in-out;
-    color: ${theme.colors.dark};
-    font-weight: 600;
-  }
-  .activeLink {
-    &:before {
-      height: 100% !important;
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    a {
+        text-decoration: none;
+        margin: 10px 0;
+        position: relative;
+        font-size: 25px;
+        transition: 0.1s ease-in-out;
+        color: ${theme.colors.dark};
+        font-weight: 600;
     }
-  }
+    .activeLink {
+        &:before {
+            height: 100% !important;
+        }
+    }
 `;
 
 export default Layout;
--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -4,103 +4,109 @@ import TileGrid from "../components/TileGrid";
 import styled from "styled-components";
 
 interface ProjectInterface {
-  name: string;
-  desc: string;
-  github?: string;
-  website?: string;
+    name: string;
+    desc: string;
+    github?: string;
+    website?: string;
 }
 
 import ProjectJSON from "../content/projects.json";
 import { graphql, useStaticQuery } from "gatsby";
 
 const query = graphql`
-  query ProjectsQuery {
-    allProjectsJson {
-      edges {
-        node {
-          name
-          desc
-          github
-          website
+    query ProjectsQuery {
+        allProjectsJson {
+            edges {
+                node {
+                    name
+                    desc
+                    github
+                    website
+                }
+            }
         }
-      }
     }
-  }
 `;
 
 export default function ProjectsPage() {
-  const projects: ProjectInterface[] = useStaticQuery(query)?.allProjectsJson.edges.map(
-    (x) => x.node
-  );
+    const projects: ProjectInterface[] = useStaticQuery(query)?.allProjectsJson.edges.map((x) => x.node);
 
-  return (
-    <Layout>
-      <Projects>
-        <h1>Projects</h1>
-        <p>Here's a collection of stuff I'm working on:</p>
-        <div className="projectList">
-          {projects.map((proj: ProjectInterface, i) => (
-            <div className="project" key={i}>
-              <span>
-                <h3>{proj.name}</h3>
-              </span>
-              <p>{proj.desc}</p>
-              <div>
-                {proj.github && <a href={proj.github}>Github</a>}
-                {proj.website && <a href={proj.website}>Website</a>}
-              </div>
-            </div>
-          ))}
-        </div>
-      </Projects>
-    </Layout>
-  );
+    return (
+        <Layout>
+            <Projects>
+                <h1>Projects</h1>
+                <p>Here's a collection of stuff I'm working on:</p>
+                <div className="projectList">
+                    {projects.map((proj: ProjectInterface, i) => (
+                        <div className="project" key={i}>
+                            <span>
+                                <h3>{proj.name}</h3>
+                            </span>
+                            <p>{proj.desc}</p>
+                            <div>
+                                {proj.github && (
+                                    <a target="_blank" href={proj.github}>
+                                        Github
+                                    </a>
+                                )}
+                                {proj.website && (
+                                    <a target="_blank" href={proj.website}>
+                                        Website
+                                    </a>
+                                )}
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            </Projects>
+        </Layout>
+    );
 }
 
 const Projects = styled.div`
-  background: #fefff0;
-  width: 100%;
-  min-height: 100%;
-  border: 15px solid #73793f1c;
-  padding: 55px;
+    background: #fefff0;
+    width: 100%;
+    min-height: 100%;
+    border: 15px solid #73793f1c;
+    padding: 55px;
 
-  @media screen and (max-width: 567px) {
-    padding: 20px;
-  }
-
-  .projectList {
-    display: grid;
-    grid-template-columns: 1fr 1fr;
-    grid-gap: 25px;
-    margin-top: 30px;
-
-    @media screen and (max-width: 992px) {
-      grid-template-columns: 1fr;
+    @media screen and (max-width: 567px) {
+        padding: 20px;
     }
-  }
 
-  .project {
-    > div {
-      padding-top: 8px;
+    .projectList {
+        display: grid;
+        grid-template-columns: 1fr 1fr;
+        grid-gap: 25px;
+        margin-top: 30px;
+
+        @media screen and (max-width: 992px) {
+            grid-template-columns: 1fr;
+        }
     }
-  }
 
-  a {
-    margin-right: 15px;
-  }
+    .project {
+        > div {
+            padding-top: 8px;
+        }
+    }
 
-  p {
-    margin-top: 8px;
-  }
+    a {
+        margin-right: 15px;
+    }
 
-  p,
-  a {
-    font-family: "Roboto Condensed", sans-serf;
-  }
+    p {
+        margin-top: 8px;
+    }
 
-  span {
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-  }
+    p,
+    a {
+        font-family: "Roboto Condensed", sans-serf;
+    }
+
+    span {
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+    }
 `;
